ba50c7cd001fbfb1fc17177433098f0f8d24bb08
Alex <alex.shillingford@icloud.com>
2025-11-17 21:44:11 -0700

    Update drizzle config file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,9 @@
-import { config } from 'dotenv';
+import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
-// Load environment variables from the correct .env file
-const env = process.env.NODE_ENV || 'development';
-// config({ path: `./.env.${env}` });
-
-// Check for the required DATABASE_URL
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not set in the environment variables');
 }
-
 
 export default defineConfig({
   out: './drizzle/migrations',
@@ -19,5 +13,5 @@ export default defineConfig({
     url: process.env.DATABASE_URL!,
   },
   verbose: true,
-  strict: true
+  strict: true,
 });

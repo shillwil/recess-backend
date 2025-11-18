7fa38e51f7121809c3855305ce4dfab239f73195
Alex <alex.shillingford@icloud.com>
2025-11-17 21:45:17 -0700

    remove database_url checker

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,9 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set in the environment variables');
-}
+// if (!process.env.DATABASE_URL) {
+//   throw new Error('DATABASE_URL is not set in the environment variables');
+// }
 
 export default defineConfig({
   out: './drizzle/migrations',
